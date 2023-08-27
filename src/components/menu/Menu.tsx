5c029d6dfc63f6244e93e32c2376d360c2950d6a
Artur <alimayko91@gmail.com>
2023-08-27 17:38:37 +0400

    закончил стилизацию header и main

--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -1,30 +1,79 @@
 import React from 'react';
 import styled from 'styled-components';
+import {Theme} from "../../styles/Theme";
+import instagram from '../../assets/images/instagram.svg'
+import telegram from '../../assets/images/telegram.svg'
+import linkedin from '../../assets/images/linkedin.svg'
 
-export const Menu = () => {
+
+export const Menu = (props: { menuItems: Array<string> }) => {
     return (
         <StyledMenu>
             <ul>
-                <li><a href="#">Home</a></li>
-                <li><a href="#">About</a></li>
-                <li><a href="#">Tech Stack</a></li>
-                <li><a href="#">Project</a></li>
-                <li><a href="#">Contact</a></li>
+                {props.menuItems.map((item, index) => {
+                    return <li key={index}>
+                        <a href="#">{item}</a>
+                    </li>
+                })}
 
-                <ul>
-                    <li><a href="#"><img src="" alt=""/></a></li>
-                    <li><a href="#"><img src="" alt=""/></a></li>
-                    <li><a href="#"><img src="" alt=""/></a></li>
-                </ul>
+                <StyledHeaderIcon>
+                    <ul>
+                        <li><a href="#"><img src={instagram} width={"40"} height={"40"} alt="icon instagram"/></a></li>
+                        <li><a href="#"><img src={telegram} width={"40"} height={"40"} alt="icon telegram"/></a></li>
+                        <li><a href="#"><img src={linkedin} width={"40"} height={"40"} alt="icon linkedin"/></a></li>
+                    </ul>
+                </StyledHeaderIcon>
+
             </ul>
-
         </StyledMenu>
     );
 };
 
-const StyledMenu = styled.nav `
-    ul {
-      display: flex;
+const StyledMenu = styled.nav`
+  ul {
+    display: flex;
     gap: 30px;
-    }
+    align-items: center;
+  }
+
+  li a {
+    color: ${Theme.colors.fontDark};
+    position: relative;
+    text-decoration: none;
+    font-family: "DM Sans", "sans-serif";
+    font-weight: 400;
+    font-size: 20px;
+    line-height: 26px;
+  }
+
+  a:hover {
+    color: #00c0fd;
+  }
+
+  a::before {
+    content: "";
+    position: absolute;
+    display: block;
+    width: 100%;
+    height: 2px;
+    bottom: 0;
+    left: 0;
+    background: linear-gradient(270deg, rgba(231, 15, 170, 1) 35%, rgba(0, 192, 253, 1) 65%);
+    transform: scaleX(0);
+    transition: transform 0.5s ease;
+  }
+
+  a:hover::before {
+    transform: scaleX(1);
+  }
+`
+
+const StyledHeaderIcon = styled.nav`
+  a:hover {
+    filter: drop-shadow(0px 10px 42px #fff);
+  }
+
+  li a::before {
+    content: none;
+  }
 `
--- src/layout/sections/main/Main.tsx
+++ src/layout/sections/main/Main.tsx
@@ -1,21 +1,26 @@
 import React from 'react';
 import styled from 'styled-components';
 import photo from '../../../assets/images/photo.webp'
+import abstract from '../../../assets/images/abstract.png'
 import {FlexWrapper} from '../../../components/FlexWrapper'
+import {Container} from '../../../components/Container';
+
 
 export const Main = () => {
     return (
         <StyledMain>
-
-            <FlexWrapper align={"center"} justify={"space-around"}>
-                <div>
-                    <Welcome>Hello</Welcome>
-                    <Name>My name is Artur Limaiko</Name>
-                    <MainTitle>A am web developer</MainTitle>
-                </div>
-
-                <Photo src={photo}/>
-            </FlexWrapper>
+            <Container>
+                <FlexWrapper align={"center"} justify={"space-around"}>
+                    <div>
+                        <Welcome>Hello👋,</Welcome>
+                        <Name>My name is <br/>Artur Limaiko</Name>
+                        <MainTitle>A am web developer</MainTitle>
+                    </div>
+                    <Frame>
+                        <Photo src={photo} alt="photo developer"/>
+                    </Frame>
+                </FlexWrapper>
+            </Container>
         </StyledMain>
     );
 };
@@ -23,29 +28,73 @@ export const Main = () => {
 const StyledMain = styled.div`
   min-height: 100vh;
   background-color: #191919;
+  display: flex;
+`
+
+
+const Frame = styled.div`
+  position: relative;
+  z-index: 0;
+
+  &::before {
+    content: "";
+    position: absolute;
+    width: 360px;
+    height: 360px;
+    background: linear-gradient(180deg, rgba(231, 15, 170, 1) 30%, rgba(0, 192, 253, 1) 70%);
+    border-radius: 50%;
+    top: -10px;
+    right: -10px;
+    z-index: -1;
+  }
+
+  &::after {
+    content: "";
+    position: absolute;
+    display: inline-block;
+    background-image: url(${abstract});
+    width: 621px;
+    height: 621px;
+    bottom: -120px;
+    right: -133px;
+    z-index: 3;
+  }
 `
 
 const Photo = styled.img`
-  width: 350px;
-  height: 350px;
+  width: 340px;
+  height: 340px;
   object-fit: cover;
+  border-radius: 50%;
 `
+
 
 const MainTitle = styled.h1`
   color: #D9D9D9;
-  text-shadow: 6px 0 5px #ffffff;
+  font-weight: bold;
+  font-family: "Poppins", "sans-serif";
+  font-size: 58px;
+  line-height: 70px;
+  letter-spacing: -1px;
 `
 
-const Name = styled.h2 `
-  background: linear-gradient(270deg, rgba(231,15,170,1) 15%, rgba(0,192,253,1) 35%);
+const Name = styled.h2`
+  background: linear-gradient(270deg, rgba(231, 15, 170, 1) 39%, rgba(0, 192, 253, 1) 70%);
   -webkit-background-clip: text;
   -webkit-text-fill-color: transparent;
-  font-size: 30px;
+  font-weight: bold;
+  font-family: "Poppins", "sans-serif";
+  font-size: 58px;
+  line-height: 70px;
+  letter-spacing: -1px;
+  margin: 10px 0;
 `
 
-const Welcome = styled.span `
+const Welcome = styled.span`
   color: #D9D9D9;
-  font-size: 30px;
   font-weight: bold;
-  text-shadow: 6px 0 5px #ffffff;
+  font-family: "Poppins", "sans-serif";
+  font-size: 58px;
+  line-height: 70px;
+  letter-spacing: -1px;
 `
