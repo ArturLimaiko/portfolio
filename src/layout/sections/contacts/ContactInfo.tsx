6723210004a0f2d1358351c758386e7c1603c0d4
Artur <alimayko91@gmail.com>
2023-08-28 18:02:57 +0400

    исправил ошибки при пуше в компоненте contacts

--- src/layout/sections/contacts/ContactInfo.tsx
+++ src/layout/sections/contacts/ContactInfo.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import styled from "styled-components";
 import {Icon} from "../../../components/icon/Icon";
 
-
 export const ContactsInfo = () => {
     return (
         <ContactInfo>
