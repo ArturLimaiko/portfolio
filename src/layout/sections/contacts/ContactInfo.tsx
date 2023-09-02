ee17bcb158008ca9420c9517e39a1bd669ea7f41
Artur <alimayko91@gmail.com>
2023-09-02 08:42:38 +0300

    перезалил иконки в компоненте Contacts

--- src/layout/sections/contacts/ContactInfo.tsx
+++ src/layout/sections/contacts/ContactInfo.tsx
@@ -9,18 +9,18 @@ export const ContactsInfo = () => {
             <ul>
                 <li>
                     <ContactLink>
-                        <Icon iconId={"phone"} viewBox={"0 0 24 24"} fill={"#000"} width={"80"} height={"80"}/>
+                        <Icon iconId={"phone"} viewBox={"0 0 41.167 41.167"} fill={"#000"} width={"80"} height={"80"}/>
                         +1 000 992 2222
                     </ContactLink>
                 </li>
                 <li>
                     <ContactLink>
-                        <Icon iconId={"mail"} viewBox={"0 0 24 24"} fill={"#000"} width={"80"} height={"80"}/>
+                        <Icon iconId={"mail"} viewBox={"0 0 512 512"} fill={"#000"} width={"80"} height={"80"}/>
                         info@domainname.com
                     </ContactLink>
                 </li>
                 <li>
-                    <ContactLink> <Icon iconId={"#"}/>
+                    <ContactLink> <Icon iconId={"location"} viewBox={"0 0 64 64"} fill="#000" width={"80"} height={"80"}/>
                         New-York Sity
                     </ContactLink>
                 </li>
@@ -43,4 +43,14 @@ const ContactInfo = styled.div`
 const ContactLink = styled.a`
   color: #cccccc;
   cursor: pointer;
+  
+  &:hover {
+    color: #00c0fd;
+
+    &:hover svg {
+      fill: #00c0fd;;
+    }
+  }
+  
+
 `
