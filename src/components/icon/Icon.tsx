871fdf80e07a8ce7f3f6a7cbef4dfbc5592dd000
Artur <alimayko91@gmail.com>
2023-09-13 00:12:42 +0300

    общие правки в файлах

--- src/components/icon/Icon.tsx
+++ src/components/icon/Icon.tsx
@@ -9,10 +9,10 @@ type IconPropsType = {
     fill?: string
 }
 
-export const Icon = (props: IconPropsType) => {
+export const Icon: React.FC<IconPropsType> = (props: IconPropsType) => {
     return (
         <svg width={props.width || "120"} height={props.height || "120"} viewBox={props.viewBox || "0 0 120 120"}
-             fill={props.fill || "none"}  xmlns="http://www.w3.org/2000/svg">
+             fill={props.fill || "none"} xmlns="http://www.w3.org/2000/svg">
             <use xlinkHref={`${iconsSprite}#${props.iconId}`}/>
         </svg>
     );
